f505b2b5e0c122ce888e3d4f302ab3805d2185bf
Saahil Joshi <saahiljoshi@ucsb.edu>
2022-10-28 14:15:13 -0700

    sj - fixed expected var type error

--- src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationsControllerTests.java
@@ -113,7 +113,7 @@ public class UCSBOrganizationsControllerTests extends ControllerTestCase {
 
             // arrange
 
-            when(ucsbOrganizationsRepository.findById(eq(7L))).thenReturn(Optional.empty());
+            when(ucsbOrganizationsRepository.findById("")).thenReturn(Optional.empty());
 
             // act
             MvcResult response = mockMvc.perform(get("/api/ucsborganizations?id=7"))
@@ -169,8 +169,8 @@ public class UCSBOrganizationsControllerTests extends ControllerTestCase {
             // arrange
             UCSBOrganizations ucsbOrg1 = UCSBOrganizations.builder()
                             .orgCode("LBJ")
-                            .orgTranslationShort("LeBr Ja")
-                            .orgTranslation("LeBron James")
+                            .orgTranslationShort("LeBrJa")
+                            .orgTranslation("LeBronJames")
                             .inactive(false)
                             .build();
 
@@ -178,13 +178,13 @@ public class UCSBOrganizationsControllerTests extends ControllerTestCase {
 
             // act
             MvcResult response = mockMvc.perform(
-                            post("/api/ucsbdates/post?name=firstDayOfClasses&quarterYYYYQ=20222&localDateTime=2022-01-03T00:00:00")
+                            post("/api/ucsborganizations/post?orgCode=LBJ&orgTranslationShort=LeBrJa&orgTranslation=LeBronJames")
                                             .with(csrf()))
                             .andExpect(status().isOk()).andReturn();
 
             // assert
-            verify(ucsbDateRepository, times(1)).save(ucsbDate1);
-            String expectedJson = mapper.writeValueAsString(ucsbDate1);
+            verify(ucsbOrganizationsRepository, times(1)).save(ucsbOrg1);
+            String expectedJson = mapper.writeValueAsString(ucsbOrg1);
             String responseString = response.getResponse().getContentAsString();
             assertEquals(expectedJson, responseString);
     }
@@ -193,78 +193,74 @@ public class UCSBOrganizationsControllerTests extends ControllerTestCase {
     @Test
     public void admin_can_delete_a_date() throws Exception {
             // arrange
-
-            LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
-
-            UCSBDate ucsbDate1 = UCSBDate.builder()
-                            .name("firstDayOfClasses")
-                            .quarterYYYYQ("20222")
-                            .localDateTime(ldt1)
+            UCSBOrganizations ucsbOrg1 = UCSBOrganizations.builder()
+                            .orgCode("LBJ")
+                            .orgTranslationShort("LeBrJa")
+                            .orgTranslation("LeBronJames")
+                            .inactive(false)
                             .build();
 
-            when(ucsbDateRepository.findById(eq(15L))).thenReturn(Optional.of(ucsbDate1));
+            when(ucsbOrganizationsRepository.findById("LBJ")).thenReturn(Optional.of(ucsbOrg1));
 
             // act
             MvcResult response = mockMvc.perform(
-                            delete("/api/ucsbdates?id=15")
+                            delete("/api/ucsborganizations?id=LBJ")
                                             .with(csrf()))
                             .andExpect(status().isOk()).andReturn();
 
             // assert
-            verify(ucsbDateRepository, times(1)).findById(15L);
-            verify(ucsbDateRepository, times(1)).delete(any());
+            verify(ucsbOrganizationsRepository, times(1)).findById("LBJ");
+            verify(ucsbOrganizationsRepository, times(1)).delete(any());
 
             Map<String, Object> json = responseToJson(response);
-            assertEquals("UCSBDate with id 15 deleted", json.get("message"));
+            assertEquals("UCSBOrganizations with id LBJ deleted", json.get("message"));
     }
 
     @WithMockUser(roles = { "ADMIN", "USER" })
     @Test
-    public void admin_tries_to_delete_non_existant_ucsbdate_and_gets_right_error_message()
+    public void admin_tries_to_delete_non_existant_ucsborganization_and_gets_right_error_message()
                     throws Exception {
             // arrange
 
-            when(ucsbDateRepository.findById(eq(15L))).thenReturn(Optional.empty());
+            when(ucsbOrganizationsRepository.findById("lbj")).thenReturn(Optional.empty());
 
             // act
             MvcResult response = mockMvc.perform(
-                            delete("/api/ucsbdates?id=15")
+                            delete("/api/ucsborganizations?id=lbj")
                                             .with(csrf()))
                             .andExpect(status().isNotFound()).andReturn();
 
             // assert
-            verify(ucsbDateRepository, times(1)).findById(15L);
+            verify(ucsbOrganizationsRepository, times(1)).findById("lbj"));
             Map<String, Object> json = responseToJson(response);
-            assertEquals("UCSBDate with id 15 not found", json.get("message"));
+            assertEquals("UCSBOrganization with id lbj not found", json.get("message"));
     }
 
     @WithMockUser(roles = { "ADMIN", "USER" })
     @Test
-    public void admin_can_edit_an_existing_ucsbdate() throws Exception {
+    public void admin_can_edit_an_existing_ucsborganization() throws Exception {
             // arrange
-
-            LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
-            LocalDateTime ldt2 = LocalDateTime.parse("2023-01-03T00:00:00");
-
-            UCSBDate ucsbDateOrig = UCSBDate.builder()
-                            .name("firstDayOfClasses")
-                            .quarterYYYYQ("20222")
-                            .localDateTime(ldt1)
+            UCSBOrganizations ucsbOrgOrig = UCSBOrganizations.builder()
+                            .orgCode("LBJ")
+                            .orgTranslationShort("LeBrJa")
+                            .orgTranslation("LeBronJames")
+                            .inactive(false)
                             .build();
 
-            UCSBDate ucsbDateEdited = UCSBDate.builder()
-                            .name("firstDayOfFestivus")
-                            .quarterYYYYQ("20232")
-                            .localDateTime(ldt2)
+            UCSBOrganizations ucsbOrgEdited = UCSBOrganizations.builder()
+                            .orgCode("STC")
+                            .orgTranslationShort("SteCur")
+                            .orgTranslation("StephCurry")
+                            .inactive(false)
                             .build();
 
-            String requestBody = mapper.writeValueAsString(ucsbDateEdited);
+            String requestBody = mapper.writeValueAsString(ucsbOrgEdited);
 
-            when(ucsbDateRepository.findById(eq(67L))).thenReturn(Optional.of(ucsbDateOrig));
+            when(ucsbOrganizationsRepository.findById("LBJ")).thenReturn(Optional.of(ucsbOrgOrig));
 
             // act
             MvcResult response = mockMvc.perform(
-                            put("/api/ucsbdates?id=67")
+                            put("/api/ucsborganizations?id=LBJ")
                                             .contentType(MediaType.APPLICATION_JSON)
                                             .characterEncoding("utf-8")
                                             .content(requestBody)
@@ -272,8 +268,8 @@ public class UCSBOrganizationsControllerTests extends ControllerTestCase {
                             .andExpect(status().isOk()).andReturn();
 
             // assert
-            verify(ucsbDateRepository, times(1)).findById(67L);
-            verify(ucsbDateRepository, times(1)).save(ucsbDateEdited); // should be saved with correct user
+            verify(ucsbOrganizationsRepository, times(1)).findById("LBJ");
+            verify(ucsbOrganizationsRepository, times(1)).save(ucsbOrgEdited); // should be saved with correct user
             String responseString = response.getResponse().getContentAsString();
             assertEquals(requestBody, responseString);
     }
@@ -282,22 +278,20 @@ public class UCSBOrganizationsControllerTests extends ControllerTestCase {
     @Test
     public void admin_cannot_edit_ucsbdate_that_does_not_exist() throws Exception {
             // arrange
-
-            LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
-
-            UCSBDate ucsbEditedDate = UCSBDate.builder()
-                            .name("firstDayOfClasses")
-                            .quarterYYYYQ("20222")
-                            .localDateTime(ldt1)
+            UCSBOrganizations ucsbEditedDate = UCSBOrganizations.builder()
+                            .orgCode("LBJ")
+                            .orgTranslationShort("LeBrJa")
+                            .orgTranslation("LeBronJames")
+                            .inactive(false)
                             .build();
 
             String requestBody = mapper.writeValueAsString(ucsbEditedDate);
 
-            when(ucsbDateRepository.findById(eq(67L))).thenReturn(Optional.empty());
+            when(ucsbOrganizationsRepository.findById("LBJ")).thenReturn(Optional.empty());
 
             // act
             MvcResult response = mockMvc.perform(
-                            put("/api/ucsbdates?id=67")
+                            put("/api/ucsborganizations?id=67")
                                             .contentType(MediaType.APPLICATION_JSON)
                                             .characterEncoding("utf-8")
                                             .content(requestBody)
@@ -305,7 +299,7 @@ public class UCSBOrganizationsControllerTests extends ControllerTestCase {
                             .andExpect(status().isNotFound()).andReturn();
 
             // assert
-            verify(ucsbDateRepository, times(1)).findById(67L);
+            verify(ucsbOrganizationsRepository, times(1)).findById(67L);
             Map<String, Object> json = responseToJson(response);
             assertEquals("UCSBDate with id 67 not found", json.get("message"));
 
